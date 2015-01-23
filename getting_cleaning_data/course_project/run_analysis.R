0a55ead31110cc96c0c84ce55876df272dfe92e7
Daniel Godoy Gómez <dgodoy0@hotmail.com>
2015-01-22 21:59:46 -0500

    correct write.table
    Changed write.csv to write.table in order to have a .txt output

--- getting_cleaning_data/course_project/run_analysis.R
+++ getting_cleaning_data/course_project/run_analysis.R
@@ -69,7 +69,7 @@ colnames(merged_data) <- c("subject", "actv", features[finalselection])
 head(merged_data)
 
 #export data
-write.csv(merged_data, "merged_data.csv")
+write.table(merged_data, "merged_data.txt")
 
 #STEP5:From the data set in step 4, creates a second, independent tidy data set with the #average of each variable for each activity and each subject.
 
@@ -81,7 +81,7 @@ tidy_data <-aggregate(. ~subject + actv, merged_data, mean)
 head(tidy_data)
 
 #export data
-write.csv(tidy_data, "tidy_data.csv")
+write.table(tidy_data, "tidy_data.txt")
 
 #END
 
